7054aab8109592669b227b7266f0843425723a9e
skqwk <semen.esaev@yandex.ru>
2024-08-23 07:23:25 +0300

    main - Включение DiscoveryConfig

--- services/common/src/main/java/ru/rockutor/discovery/DiscoveryConfig.java
+++ services/common/src/main/java/ru/rockutor/discovery/DiscoveryConfig.java
@@ -1,11 +1,18 @@
 package ru.rockutor.discovery;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 @EnableDiscoveryClient
 @ConditionalOnProperty(name = "eureka.enable.discovery", havingValue = "true")
 public class DiscoveryConfig {
+    @PostConstruct
+    void run() {
+        log.info("Eureka enable discovery");
+    }
 }
--- services/editor/src/main/java/ru/rockutor/editor/EditorApp.java
+++ services/editor/src/main/java/ru/rockutor/editor/EditorApp.java
@@ -3,6 +3,7 @@ package ru.rockutor.editor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration;
 
 /**
  * Исключаем SecurityAutoConfiguration,
